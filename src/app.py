94a10a79f5a8feeb33c18f9d7dbdcf374df5d516
Carlos Bustamante <carlos.bustamante.sequeiros@gmai.com>
2023-05-07 16:08:55 +0200

    Me voy a sevilla

--- src/app.py
+++ src/app.py
@@ -1,17 +1,27 @@
 from flask import Flask, jsonify
 from flask_pymongo import PyMongo
+from flask_restplus import Api, Resource
 from bson.json_util import dumps
 
 
-
 app = Flask(__name__)
+api = Api(app)
+
 app.config.from_prefixed_env()
 app.config['MONGO_URI']
 mongo = PyMongo(app)
 
 
 
-@app.get('/')
-def home ():
-    vgames = mongo.db.games.find()
-    
+
+@api.route('/season')
+class Season(Resource):
+    def get(self):
+        return 'Hello'
+
+
+
+
+#mongo.db.games.find()
+
+#app.add_url_rule()
