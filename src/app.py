4564b299c214d3052978e2e1f68997576c380dee
Buzztayaki <carlos.bustamante.sequeiros@gmail.com>
2023-05-09 19:10:38 +0200

    Leves cambios, uso de flask restx

--- src/app.py
+++ src/app.py
@@ -1,23 +1,26 @@
 from flask import Flask, jsonify
+import db
 from flask_pymongo import PyMongo
-from flask_restplus import Api, Resource
+from flask_restx import Api, Resource
 from bson.json_util import dumps
 
 
 app = Flask(__name__)
-api = Api(app)
-
 app.config.from_prefixed_env()
 app.config['MONGO_URI']
+
+api = Api(app)
 mongo = PyMongo(app)
 
 
 
 
-@api.route('/season')
+@api.route('/inicio')
 class Season(Resource):
     def get(self):
-        return 'Hello'
+        # data = mongo.db.football.find({'Country': 'Spain'})
+        # print(data)
+        return {"hola": "hola"}
 
 
 
