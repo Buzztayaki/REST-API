c2989574d1451b4bf0fa77ae95e19926e2710942
Carlos Bustamante <carlos.bustamante.sequeiros@gmai.com>
2023-05-09 21:50:23 +0200

    Espero que no se haya borrado nada

--- src/app.py
+++ src/app.py
@@ -1,13 +1,11 @@
 from flask import Flask, jsonify
 import db
 from flask_pymongo import PyMongo
-from flask_restplus import Api, Resource
+from flask_restx import Api, Resource
 from bson.json_util import dumps
 
 
 app = Flask(__name__)
-api = Api(app)
-
 app.config.from_prefixed_env()
 app.config['MONGO_URI']
 
@@ -17,13 +15,12 @@ mongo = PyMongo(app)
 
 
 
-@api.route('/season')
+@api.route('/inicio')
 class Season(Resource):
     def get(self):
-        return 'Hello'
-
-
-
+        # data = mongo.db.football.find({'Country': 'Spain'})
+        # print(data)
+        return {"hola": "hola"}
 
 #mongo.db.games.find()
 
