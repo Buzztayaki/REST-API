570b4cb83de78ddd19d3c50a82ef46b9ff259405
Carlos Bustamante <carlos.bustamante.sequeiros@gmai.com>
2023-05-09 21:44:16 +0200

    Me voy a sevilla

--- src/app.py
+++ src/app.py
@@ -1,11 +1,9 @@
 from flask import Flask, jsonify
 from flask_pymongo import PyMongo
-from flask_restplus import Api, Resource
 from bson.json_util import dumps
 
 
 app = Flask(__name__)
-api = Api(app)
 
 app.config.from_prefixed_env()
 app.config['MONGO_URI']
@@ -13,11 +11,6 @@ mongo = PyMongo(app)
 
 
 
-
-@api.route('/season')
-class Season(Resource):
-    def get(self):
-        return 'Hello'
 
 
 
