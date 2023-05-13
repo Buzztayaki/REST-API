efc3ad8e7917393b4e598e643be62c31f8e1d334
Buzztayaki <carlos.bustamante.sequeiros@gmail.com>
2023-05-13 18:22:23 +0200

    Esctructura estandar

--- src/app.py
+++ src/app.py
@@ -1,5 +1,4 @@
 from flask import Flask, jsonify, Response
-import json
 from flask_pymongo import PyMongo
 from flask_restx import Api, Resource
 from bson.json_util import dumps
@@ -18,12 +17,18 @@ class AllMatches(Resource):
     def get(self):
         data = dumps(mongo.db.matches.find())
         return Response(data, mimetype='aplication/json')
+    def post(self):
+        pass
 
 @api.route('/matches/<string:id>')
 class OneMatch(Resource):
     def get(self, id):
         data = dumps(mongo.db.matches.find_one_or_404({'_id': "ObjectId({})".format(id)}))
         return Response(data, mimetype='aplication/json')
+    def put(self, id):
+        pass
+    def delete(self, id):
+        pass
 
 #mongo.db.games.find()
 
