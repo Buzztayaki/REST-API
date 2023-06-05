d8d6e8d566d2b38405854b771ca789a3c18ee3c9
Carlos Bustamante <carlos.bustamante.sequeiros@gmai.com>
2023-06-05 16:33:50 +0200

    Imagenes

--- web/index.php
+++ web/index.php
@@ -10,9 +10,18 @@
     <div class="min-h-screen flex flex-col justify-center items-center">
         <h1 class="text-4xl text-white">Football Data Page</h1>
         <div class="flex mt-4">
-            <a href="./matches.php" class="bg-yellow-500 hover:bg-yellow-600 text-white font-semibold py-2 px-4 rounded-full mr-2 w-24 h-10">Partidos</a>
-            <a href="./classification.php" class="bg-yellow-500 hover:bg-yellow-600 text-white font-semibold py-2 px-4 rounded-full mr-2">Clasificación</a>
-            <a href="./charts.php" class="bg-yellow-500 hover:bg-yellow-600 text-white font-semibold py-2 px-4 rounded-full">Gráficas</a>
+            <a href="./matches.php" class="relative flex flex-col bg-yellow-500 hover:bg-yellow-600 text-white font-semibold py-2 px-4 rounded-lg mr-2 w-40">
+                <img src="ruta-a-tu-imagen-1.jpg" alt="Partidos" class="object-cover h-24 rounded-t-lg">
+                <span class="mt-auto text-center">Partidos</span>
+            </a>
+            <a href="./classification.php" class="relative flex flex-col bg-yellow-500 hover:bg-yellow-600 text-white font-semibold py-2 px-4 rounded-lg mr-2 w-40">
+                <img src="ruta-a-tu-imagen-2.jpg" alt="Clasificación" class="object-cover h-24 rounded-t-lg">
+                <span class="mt-auto text-center">Clasificación</span>
+            </a>
+            <a href="./charts.php" class="relative flex flex-col bg-yellow-500 hover:bg-yellow-600 text-white font-semibold py-2 px-4 rounded-lg w-40">
+                <img src="ruta-a-tu-imagen-3.jpg" alt="Gráficas" class="object-cover h-24 rounded-t-lg">
+                <span class="mt-auto text-center">Gráficas</span>
+            </a>
         </div>
     </div>
 </body>
