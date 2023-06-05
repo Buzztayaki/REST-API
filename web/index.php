b677286c2e03eb832c3a554744ddc2a027b05b22
Carlos Bustamante <carlos.bustamante.sequeiros@gmai.com>
2023-06-05 17:08:42 +0200

    Cambios en index.php

--- web/index.php
+++ web/index.php
@@ -6,20 +6,20 @@
     <title>Página de Inicio</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.16/dist/tailwind.min.css" rel="stylesheet">
 </head>
-<body style="background-image: url('./img/Fondo4.jpg'); background-size: cover; background-position: center;">
+<body style="background-image: url('./img/fondo10.jpeg'); background-size: cover; background-position: center;">
     <div class="min-h-screen flex flex-col justify-center items-center">
         <h1 class="text-4xl text-white">Football Data Page</h1>
         <div class="flex mt-4">
-            <a href="./matches.php" class="relative flex flex-col bg-yellow-500 hover:bg-yellow-600 text-white font-semibold py-2 px-4 rounded-lg mr-2 w-40">
-                <img src="ruta-a-tu-imagen-1.jpg" alt="Partidos" class="object-cover h-24 rounded-t-lg">
+            <a href="./matches.php" class="relative flex flex-col bg-white bg-opacity-50 hover:bg-opacity-75 text-white font-semibold py-2 px-4 rounded-lg mr-2 w-40">
+                <img src="./img/matches.jpg" alt="Partidos" class="object-cover h-24 rounded-t-lg">
                 <span class="mt-auto text-center">Partidos</span>
             </a>
-            <a href="./classification.php" class="relative flex flex-col bg-yellow-500 hover:bg-yellow-600 text-white font-semibold py-2 px-4 rounded-lg mr-2 w-40">
-                <img src="ruta-a-tu-imagen-2.jpg" alt="Clasificación" class="object-cover h-24 rounded-t-lg">
+            <a href="./classification.php" class="relative flex flex-col bg-white bg-opacity-50 hover:bg-opacity-75 text-white font-semibold py-2 px-4 rounded-lg mr-2 w-40">
+                <img src="./img/cup.jpg" alt="Clasificación" class="object-cover h-24 rounded-t-lg">
                 <span class="mt-auto text-center">Clasificación</span>
             </a>
-            <a href="./charts.php" class="relative flex flex-col bg-yellow-500 hover:bg-yellow-600 text-white font-semibold py-2 px-4 rounded-lg w-40">
-                <img src="ruta-a-tu-imagen-3.jpg" alt="Gráficas" class="object-cover h-24 rounded-t-lg">
+            <a href="./charts.php" class="relative flex flex-col bg-white bg-opacity-50 hover:bg-opacity-75 text-white font-semibold py-2 px-4 rounded-lg w-40">
+                <img src="./img/graficas.png" alt="Gráficas" class="object-cover h-24 rounded-t-lg">
                 <span class="mt-auto text-center">Gráficas</span>
             </a>
         </div>
